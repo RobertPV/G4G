6e35938fbe8f0d7a7450df24ae26bd09fdf54ee8
Roberto PV <durazznit0.yt@gmail.com>
2023-01-03 20:07:25 -0600

    Ejercicio de clases y objetos, en la carpeta Ejercicio04, se crea la clase Coche y en Program creamos 5 objetos tipo Coche

---
+++ Ejercicio04/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ejercicio04
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Concesionaria concesionaria = new Concesionaria(5);
+
+            Coche coche1 = new Coche(1,"KIA","Rio",110,250000);
+            Coche coche2 = new Coche(2,"Chevrolet","Beat",100,200000);
+            Coche coche3 = new Coche(3,"JAC","e10x",90,450000);
+            Coche coche4 = new Coche(4,"Nissan","March",100,280000);
+            Coche coche5 = new Coche(5,"Volkswagen","Jetta",120,350000);
+
+            concesionaria.añadirCoche(coche1);
+            concesionaria.añadirCoche(coche2);
+            concesionaria.añadirCoche(coche3);
+            concesionaria.añadirCoche(coche4);
+            concesionaria.añadirCoche(coche5);
+
+            concesionaria.mostrarCoches();
+        }
+    }
+}
