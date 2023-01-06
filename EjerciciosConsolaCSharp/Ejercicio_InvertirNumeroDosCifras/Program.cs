9f8aedb0d2a1030c898c882868a69beb9e3494a3
Roberto PV <durazznit0.yt@gmail.com>
2023-01-06 13:25:01 -0600

    Se crea ejercicio de invertir un numero de dos digitos

---
+++ EjerciciosConsolaCSharp/Ejercicio_InvertirNumeroDosCifras/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            string numero = "";
+
+            do {
+                Console.WriteLine("Ingresa un numero de dos digitos: ");
+                numero = Console.ReadLine();
+                if(numero.Length > 2 || numero.Length < 2)
+                {
+                    numero = "";
+                    Console.WriteLine("Debe ser un numero de dos digitos");
+                }
+            } while (numero == "");
+
+            string invertido = "";
+            
+            for(int i = numero.Length-1; i >= 0; i--)
+            {
+                invertido += numero[i];
+            }
+
+            Console.WriteLine(int.Parse(invertido));
+
+        }
+    }
+}
