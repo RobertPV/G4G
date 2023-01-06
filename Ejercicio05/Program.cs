531eea28dfa3439d5a415156e7d5bc7ff05ccf78
Roberto PV <durazznit0.yt@gmail.com>
2023-01-05 23:24:56 -0600

    Ejercicio de clases y objetos, creando unas funciones que generan un numero aleatorio, una lista de numeros aleatorios y una lista de numero aleatorios no repetidos, desde .Net6.0

---
+++ Ejercicio05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ejercicio05
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            Aleatorios aleatorios = new Aleatorios();
+
+            Console.WriteLine(aleatorios.numeroRandom(0,100));
+            Console.WriteLine();
+
+            int [] lista = aleatorios.listaNumerosAleatorios(10,0,25);
+
+            Array.Sort(lista);
+            
+            for(int i = 0; i < lista.Length; i++)
+            {
+                Console.WriteLine(lista[i]);
+            }
+        }
+    }
+}
