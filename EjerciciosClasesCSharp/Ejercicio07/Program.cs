9b9771ca5f11636dc7bb4231b583fe5f759fb5bb
Roberto PV <durazznit0.yt@gmail.com>
2023-01-13 18:28:32 -0600

    Se crea Ejercicio07 para practicar

---
+++ EjerciciosClasesCSharp/Ejercicio07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ejercicio07
+{
+    class Program
+    {
+        static void Main(String [] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+        }
+    }
+}
